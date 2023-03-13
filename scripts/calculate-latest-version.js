6ab3165c4fe3911d8c84ccc380918b950058dda9
Oleksii Popov <Oleksii.Popov@tomtom.com>
2023-03-13 11:55:53 +0100

    ci: using GHA to implement ci-cd with feature branches

---
+++ scripts/calculate-latest-version.js
@@ -0,0 +1,39 @@
+const [, , ...args] = process.argv;
+const [localVersion, remoteVersion] = args;
+
+/*
+ Given a version "1.2.3-ALIAS_NAME.0":
+ a match call returns ["1.2.3-ALIAS_NAME.0", "1", "2", "3", ...]
+ */
+const versionRegex = /^(\d+)\.(\d+)\.(\d+)/;
+
+const getVersionNumbers = (version) => {
+    return (version.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
+};
+
+const resolveResult = (result) => {
+    process.stdout.write(result, "utf-8");
+    process.exit();
+};
+
+let version = "";
+
+if (!remoteVersion) {
+    // new alias or re-creation of the old one
+    version = localVersion;
+} else {
+    const [l_maj, l_min, l_patch] = getVersionNumbers(localVersion);
+    const [r_maj, r_min, r_patch] = getVersionNumbers(remoteVersion);
+
+    if (
+        l_maj > r_maj ||
+        (l_maj === r_maj && l_min > r_min) ||
+        (l_maj === r_maj && l_min === r_min && l_patch >= r_patch)
+    ) {
+        version = localVersion;
+    } else {
+        version = remoteVersion;
+    }
+}
+
+resolveResult(version);
---
+++ scripts/switch-version.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Select the latest current package version for an alias out of the local version and versions on the registry
+
+set -e
+
+export NODE_OPTIONS=--max_old_space_size=4096
+
+# Name of branch or tag that was pushed
+package_name=$1;
+ref_name=$2;
+
+previousVersion=$(cat package.json | jq -r .version);
+echo "Current package.json version is $previousVersion";
+
+# Get all versions of given ref_name from the registry.
+all_registry_versions=$(mktemp);
+npm show ${package_name} versions --json > ${all_registry_versions};
+
+# Select the latest version of given ref_name.
+# Note: getting the latest version from all versions instead of the dist-tag prevents issues with
+# pushing existing versions when re-creating aliases.
+jq_parameter='[.[] | select(test("^\\d+\\.\\d+\\.\\d+-'$ref_name'\\.\\d+$"))] | .[-1] // empty'
+latest_registry_version=$(jq -r "$jq_parameter" "$all_registry_versions");
+echo "The latest version on the registry for the current alias is $latest_registry_version";
+
+# Select the latest version out of the local version and latest registry version.
+pkg_v=$(node ./scripts/calculate-latest-version.js "$previousVersion" "$latest_registry_version")
+echo "Current selected pre-release version is $pkg_v";
+
+# Replace local version with resolved one, so the release script could calculate the next version
+tmp=$(mktemp);
+jq --arg v "$pkg_v" '.version = $v' package.json > "$tmp" && mv "$tmp" package.json
+jq --arg v "$pkg_v" '.version = $v' package-lock.json > "$tmp" && mv "$tmp" package-lock.json
